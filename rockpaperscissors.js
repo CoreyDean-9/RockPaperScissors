2142ac635e0f54ba477c464eb12e79ef9f2dc34b
Corey Collins <156260709+CoreyDean-9@users.noreply.github.com>
2025-01-20 17:22:47 -0500

    while loop for input error

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -19,22 +19,34 @@ function getComputerChoice(x) {
 console.log(getComputerChoice(x));
 
 //human stuff
-let userInput = prompt("Please enter your choice : (Rock, Paper, or Scissors): ");
+while (true) {
+    let userInput = prompt("Please enter your choice : (Rock, Paper, or Scissors): ");
 
-function getHumanChoice(userInput) {
-    
-    switch (userInput.toLowerCase()) {
-        case "rock":
-            console.log("You chose Rock!");
-            break;
-        case "paper":
-            console.log("You chose Paper!");
-            break;
-        case "scissors":
-            console.log("You chose Scirrors!");
-            break;
-        default:
-            console.log("Invalid Response. Try again!")
+    function getHumanChoice(userInput) {
+        
+        switch (userInput.toLowerCase()) {
+            case "rock":
+                console.log("You chose Rock!");
+                break;
+            case "paper":
+                console.log("You chose Paper!");
+                break;
+            case "scissors":
+                console.log("You chose Scirrors!");
+                break;
+            default:
+                console.log("Invalid Response. Try again!")
+                break;
+        }
+        
     }
+    console.log(getHumanChoice(userInput));
 }
-console.log(getHumanChoice(userInput));
+//keeping track of score. humanScore, computerScore, # of rounds?
+let humanScore = 0;
+let computerScore = 0;
+let numRounds = 0;
+
+function playRound(getComputerChoice, getHumanChoice) {
+    
+}
