200e039084cc8adc5323add4b46ac1162f03e28c
Corey Collins <156260709+CoreyDean-9@users.noreply.github.com>
2025-01-20 19:41:39 -0500

    up to the point of keeping score

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,48 +5,55 @@ console.log(x);
 //converting random number into string, may not be necessary
 function getComputerChoice(x) {
     switch(x){
-        case 1:
+        case 1://telling the function what to return
             return "Rock";
-            break;
-        case 2:
+        case 2: //telling the function what to return
             return "Paper";
-            break;
-        case 3:
+        case 3://telling the function what to return
             return "Scissors";
-        break;
     }
 }
 console.log(getComputerChoice(x));
 
 //human stuff
+// let userInput = prompt("Please enter your choice : (Rock, Paper, or Scissors): ");
+
 while (true) {
     let userInput = prompt("Please enter your choice : (Rock, Paper, or Scissors): ");
-
-    function getHumanChoice(userInput) {
-        
-        switch (userInput.toLowerCase()) {
-            case "rock":
-                console.log("You chose Rock!");
-                break;
-            case "paper":
-                console.log("You chose Paper!");
-                break;
-            case "scissors":
-                console.log("You chose Scirrors!");
-                break;
-            default:
-                console.log("Invalid Response. Try again!")
-                break;
-        }
-        
+    //if function true break function?
+    if (getHumanChoice(userInput)){
+        break;
     }
-    console.log(getHumanChoice(userInput));
 }
-//keeping track of score. humanScore, computerScore, # of rounds?
-let humanScore = 0;
-let computerScore = 0;
-let numRounds = 0;
+function getHumanChoice(userInput) {
+        
+    switch (userInput.toLowerCase()) {
+        case "rock":
+            console.log("You chose Rock!");
+            break;
+        case "paper":
+            console.log("You chose Paper!");
+            break;
+        case "scissors":
+            console.log("You chose Scirrors!");
+            break;
+        default:
+             console.log("Invalid Response. Try again!")
+             break;
+    }
+}
+console.log(getHumanChoice(userInput));
 
-function playRound(getComputerChoice, getHumanChoice) {
-    
-}
+//keeping track of score. humanScore, computerScore, # of rounds?
+// let humanScore = 0;
+// let computerScore = 0;
+// let numRounds = 0;
+
+// function playRound(getComputerChoice, getHumanChoice) {
+//     switch (userInput = Rock){
+//         case x = Rock:
+//             console.log(confirm("There was a tie this round, SHOOT again!"));
+//     }
+
+// }
+// playRound(x, userInput);
