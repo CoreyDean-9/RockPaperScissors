1489f55f89a9375e0fc5fb91525928a400c1b886
Corey Collins <156260709+CoreyDean-9@users.noreply.github.com>
2025-01-29 18:59:07 -0500

    fixed variable placements, formatted, fixed if/else-if logic

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,59 +1,77 @@
 //generating random number for computer choice
-let  x = Math.floor(Math.random() *3) + 1;
-console.log(x);
+// let  x = Math.floor(Math.random() *3) + 1;
+// console.log(x);
 
 //converting random number into string, may not be necessary
-function getComputerChoice(x) {
+function getComputerChoice() {
+    let  x = Math.floor(Math.random() *3) + 1;
     switch(x){
         case 1://telling the function what to return
-            return "Rock";
+            return "rock";
         case 2: //telling the function what to return
-            return "Paper";
+            return "paper";
         case 3://telling the function what to return
-            return "Scissors";
+            return "scissors";
     }
 }
-console.log(getComputerChoice(x));
-
-
+// console.log(getComputerChoice(x));
 
 //human stuff
-while (true) {
-    let userInput = prompt("Please enter your choice : (Rock, Paper, or Scissors): ");
-    //if function true break function
-    if (getHumanChoice(userInput)){
-        break; 
+function getHumanChoice() {
+    // prompting user
+    let userInput = prompt("Please enter Rock, Paper, or Scissors:").toLowerCase();
+    //if input is valid (RPS) return input
+    if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+        console.log("You chose: " + userInput);
+        return userInput;
     }
-}
-function getHumanChoice(userInput) {
-        
-    switch (userInput.toLowerCase()) {
-        case "rock":
-            console.log("You chose Rock!")
-            return "Rock";
-        case "paper":
-            console.log("You chose Paper!");
-            return "Paper";
-        case "scissors":
-            console.log("You chose Scissors!");
-            return "Scissors";
-        default:
-             console.log("Invalid Response. Try again!")
-             break;
+    else {
+        //if input is not valid (not RPS) return to function
+        console.log("That's literally not Rock, Paper, or Scissors...");
+        return getHumanChoice();
     }
 }
 
 //keeping track of score. humanScore, computerScore, # of rounds?
 let humanScore = 0;
 let computerScore = 0;
+let tiedScore = 0;
 let numRounds = 0;
 
-function playRound(getComputerChoice, getHumanChoice) {
-    switch (getHumanChoice) {
-        case getComputerChoice === "Rock":
-            console.log(confirm("There was a tie this round, SHOOT again!"));
-            return 
+function playRound(humanChoice, computerChoice) {
+    console.log('Computer Choice: ' + computerChoice );
+
+    if (humanChoice === computerChoice) {
+        console.log("This round is a tie");
+        tiedScore++;
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice == "paper"))
+        {
+            console.log("You win this round! Nice!");
+            humanScore++;
+        }
+        else{
+            console.log("The computer wins this round!");
+            computerScore++;
+        }
+}
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        console.log("\nRound: " + i + 1);
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+        console.log("Your Score: " + humanScore + "\n" + "Computer Score: " + computerScore + "\n" + "Tied Rounds: " + tiedScore );
     }
 
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
+    } else if (humanScore < computerScore) {
+        console.log("The computer had bested you my friend...skill issue!");
+    } else {
+        console.log("A TIE, you've met your match!");
+    }
 }
-playRound(getComputerChoice, getHumanChoice);
+playGame();
